8e4f8eaa26d9baa7e0b6c4d9da0224c48f71b887
Alexey Kosov <kasaff322@icloud.com>
2023-02-14 22:38:40 +0300

    pull request

---
+++ Restraunt/Restraunt/RestrauntApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestrauntApp.swift
+//  Restraunt
+//
+//  Created by Aleksey Kosov on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestrauntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
